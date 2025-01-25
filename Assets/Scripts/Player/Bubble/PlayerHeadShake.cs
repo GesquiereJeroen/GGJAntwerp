9486f34f6c1c5d8224c3bf39e1c91e8e0ffe9da2
JeroenGesquiere <gesquierejeroen@gmail.com>
2025-01-26 00:14:52 +0100

    removed scripts that caused build errors

--- Assets/Scripts/Player/Bubble/PlayerHeadShake.cs
+++ Assets/Scripts/Player/Bubble/PlayerHeadShake.cs
@@ -1,26 +1,22 @@
 using DG.Tweening;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerHeadShake : MonoBehaviour
 {
-    public float shakeRadius = 0.2f; // Radius of the jitter.
-    public float duration = 0.1f;    // Time for each jitter cycle.
-    private Tween jitterTween;
+	public float shakeRadius = 0.2f; // Radius of the jitter.
+	public float duration = 0.1f;    // Time for each jitter cycle.
+	private Tween jitterTween;
 
-    void Start()
-    {
-        StartJitteryShake();
+	void Start()
+	{
+		StartJitteryShake();
 
 		GameManager.Instance.GameEnded += EndGameShake;
-    }
+	}
 
 	private void EndGameShake(object sender, bool e)
 	{
-			StopJitteryShake();
+		StopJitteryShake();
 		if (!e)
 		{
 			// Stop any existing shake
@@ -33,22 +29,22 @@ public class PlayerHeadShake : MonoBehaviour
 	}
 
 	public void StartJitteryShake()
-    {
-        // Stop any existing shake
-        StopJitteryShake();
+	{
+		// Stop any existing shake
+		StopJitteryShake();
 
-        // Set up a continuous jitter effect
-        jitterTween = transform.DOShakeRotation(duration, shakeRadius, vibrato: 20, randomness: 100, fadeOut: false)
-            .SetLoops(-1, LoopType.Restart); // Infinite loop
-    }
+		// Set up a continuous jitter effect
+		jitterTween = transform.DOShakeRotation(duration, shakeRadius, vibrato: 20, randomness: 100, fadeOut: false)
+			.SetLoops(-1, LoopType.Restart); // Infinite loop
+	}
 
-    public void StopJitteryShake()
-    {
-        if (jitterTween != null)
-        {
-            jitterTween.Kill(); // Stop the shake
-            jitterTween = null;
-            //transform.localPosition = Vector3.zero; // Reset position
-        }
-    }
+	public void StopJitteryShake()
+	{
+		if (jitterTween != null)
+		{
+			jitterTween.Kill(); // Stop the shake
+			jitterTween = null;
+			//transform.localPosition = Vector3.zero; // Reset position
+		}
+	}
 }
--- Assets/Scripts/Player/Bubble/VertexWobble.cs
+++ Assets/Scripts/Player/Bubble/VertexWobble.cs
@@ -1,89 +1,86 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
-using Unity.VisualScripting;
+using UnityEngine;
 public class VertexWobble : MonoBehaviour
 {
-    TMP_Text textMesh;
-    Mesh mesh;
-    Vector3[] vertices;
-    Color32[] vertexColors;
-    // Start is called before the first frame update
-    void Start()
-    {
-        textMesh = GetComponent<TMP_Text>();
-    }
+	TMP_Text textMesh;
+	Mesh mesh;
+	Vector3[] vertices;
+	Color32[] vertexColors;
+	// Start is called before the first frame update
+	void Start()
+	{
+		textMesh = GetComponent<TMP_Text>();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        textMesh.ForceMeshUpdate();
-        mesh= textMesh.mesh;
-        vertices = mesh.vertices;
+	// Update is called once per frame
+	void Update()
+	{
+		textMesh.ForceMeshUpdate();
+		mesh = textMesh.mesh;
+		vertices = mesh.vertices;
 
-        for(int i = 0; i < textMesh.textInfo.characterCount; i++)
-        {
+		for (int i = 0; i < textMesh.textInfo.characterCount; i++)
+		{
 
-            TMP_CharacterInfo c = textMesh.textInfo.characterInfo[i];
+			TMP_CharacterInfo c = textMesh.textInfo.characterInfo[i];
 
-            int index = c.vertexIndex;
+			int index = c.vertexIndex;
 
 			index %= vertices.Length;
 
-            Vector3 offset = Wobble(Time.time+i);
+			Vector3 offset = Wobble(Time.time + i);
 
-            vertices[index] +=offset;
-            vertices[index+1] += offset;
-            vertices[index+2] += offset;
-            vertices[index+3] += offset;
-             mesh.vertices = vertices;
-            textMesh.canvasRenderer.SetMesh(mesh);
-        }
+			vertices[index] += offset;
+			vertices[index + 1] += offset;
+			vertices[index + 2] += offset;
+			vertices[index + 3] += offset;
+			mesh.vertices = vertices;
+			textMesh.canvasRenderer.SetMesh(mesh);
+		}
 
-    }
+	}
 
-    Vector2 Wobble(float time)
-    {
-        return new Vector2(Mathf.Sin(time*5f)*2,Mathf.Cos(time*5f)*2);
-    }
+	Vector2 Wobble(float time)
+	{
+		return new Vector2(Mathf.Sin(time * 5f) * 2, Mathf.Cos(time * 5f) * 2);
+	}
 
-    public void RemoveCharacter(char characterToRemove)
-    {
-        string originalText = textMesh.text;
-        string updatedText = originalText.Replace(characterToRemove.ToString(), "\u00A0");
-        textMesh.text = updatedText;
-    }
+	public void RemoveCharacter(char characterToRemove)
+	{
+		string originalText = textMesh.text;
+		string updatedText = originalText.Replace(characterToRemove.ToString(), "\u00A0");
+		textMesh.text = updatedText;
+	}
 
-    public void MakeCharacterDisappear(char characterToHide)
-    {
-        textMesh.ForceMeshUpdate();
-        mesh = textMesh.mesh;
-        vertexColors = mesh.colors32;
+	public void MakeCharacterDisappear(char characterToHide)
+	{
+		textMesh.ForceMeshUpdate();
+		mesh = textMesh.mesh;
+		vertexColors = mesh.colors32;
 
-        for (int i = 0; i < textMesh.textInfo.characterCount; i++)
-        {
-            TMP_CharacterInfo c = textMesh.textInfo.characterInfo[i];
+		for (int i = 0; i < textMesh.textInfo.characterCount; i++)
+		{
+			TMP_CharacterInfo c = textMesh.textInfo.characterInfo[i];
 
-            // Check if the character matches
-            if (c.character == characterToHide && c.isVisible)
-            {
-                int vertexIndex = c.vertexIndex;
+			// Check if the character matches
+			if (c.character == characterToHide && c.isVisible)
+			{
+				int vertexIndex = c.vertexIndex;
 
-                // Ensure the vertex index is within bounds
-                if (vertexIndex >= vertexColors.Length) continue;
+				// Ensure the vertex index is within bounds
+				if (vertexIndex >= vertexColors.Length) continue;
 
-                // Set alpha to 0 for all 4 vertices of the character
-                vertexColors[vertexIndex] = new Color32(0, 0, 0, 0); // Top-left
-                vertexColors[vertexIndex + 1] = new Color32(0, 0, 0, 0); // Top-right
-                vertexColors[vertexIndex + 2] = new Color32(0, 0, 0, 0); // Bottom-right
-                vertexColors[vertexIndex + 3] = new Color32(0, 0, 0, 0); // Bottom-left
-            }
-        }
+				// Set alpha to 0 for all 4 vertices of the character
+				vertexColors[vertexIndex] = new Color32(0, 0, 0, 0); // Top-left
+				vertexColors[vertexIndex + 1] = new Color32(0, 0, 0, 0); // Top-right
+				vertexColors[vertexIndex + 2] = new Color32(0, 0, 0, 0); // Bottom-right
+				vertexColors[vertexIndex + 3] = new Color32(0, 0, 0, 0); // Bottom-left
+			}
+		}
 
-        // Apply the updated colors to the mesh
-        mesh.colors32 = vertexColors;
-        textMesh.canvasRenderer.SetMesh(mesh);
-        Debug.Log("Here");
-    }
+		// Apply the updated colors to the mesh
+		mesh.colors32 = vertexColors;
+		textMesh.canvasRenderer.SetMesh(mesh);
+		Debug.Log("Here");
+	}
 }
